9dd8993705923db6bdc755cbf23adf455f362ee7
Iannis de Zwart <info@iannis.io>
2022-08-24 22:32:49 +0200

    📱 Add PWA Support

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 export default {
 	siteMetadata: {
 		title: 'flash-cards',
-		siteUrl: 'https://www.yourdomain.tld'
+		siteUrl: 'https://flashcards.iannis.io'
 	},
 	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
 	// If you use VSCode you can also use the GraphQL plugin
@@ -10,6 +10,19 @@ export default {
 	plugins: [
 		'gatsby-plugin-sass',
 		'gatsby-plugin-react-svg',
-		'gatsby-transformer-json'
+		'gatsby-transformer-json',
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Flashcards`,
+				short_name: `Flashcards`,
+				start_url: `/`,
+				background_color: `#232328`,
+				theme_color: `#3c8dec`,
+				display: `standalone`,
+				icon: `src/icons/favicon.png`
+			},
+		},
+		'gatsby-plugin-offline'
 	]
 }
